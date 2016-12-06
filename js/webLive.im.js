f9b76180e192f308825e7a922db4e9852eb48246
283731869@qq.com <28373869@qq.com>
2016-12-06 21:47:08 +0800

    完成页面直播信息体

--- js/webLive.im.js
+++ js/webLive.im.js
@@ -108,9 +108,9 @@
                 break;
             case webim.GROUP_MSG_SUB_TYPE.LOVEMSG://群点赞消息
                 //业务自己可以增加逻辑，比如展示点赞动画效果
-                //contentSpan.innerHTML = "[群点赞消息]" + convertMsgtoHtml(msg);
+                contentSpan.innerHTML = "[群点赞消息]" + convertMsgtoHtml(msg);
                 //展示点赞动画
-                //showLoveMsgAnimation();
+                // showLoveMsgAnimation();
                 break;
             case webim.GROUP_MSG_SUB_TYPE.TIP://群提示消息
                 //contentSpan.innerHTML = "[群提示消息]" + convertMsgtoHtml(msg);
@@ -191,7 +191,6 @@
     liveim.joinbiggroup = function(){
 
     };
-
     liveim.logout = function(){
         //退出
         webim.quitBigGroup({
